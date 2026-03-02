2fb9a81b995698b289940cc8554585073dccf9f5
michiguns2024-web <michiguns2024@gmail.com>
2026-03-01 19:40:27 -0500

    Update base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/annarborsolarinstaller/',
+  base: '/annarborsolarinstaller.com/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
